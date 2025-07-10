a9a21d0a2614306f871ef51bb9526453feff59f5
milewsk <milewski.konrad00@gmail.com>
2025-07-10 19:22:59 +0200

    Add DependencyInjection.cs to Infrastructure

---
+++ src/Infrastructure/Data/Schemas.cs
@@ -0,0 +1,6 @@
+namespace Infrastructure.Data;
+
+public class Schemas
+{
+    public const string Default = "default";
+}
---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,51 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddHealthChecks()
+            .AddNpgSql(configuration.GetConnectionString("Database"));
+
+        return services;
+    }
+
+    public static IServiceCollection AddDatabase(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<ApplicationDbContext>(
+            options => options
+                .UseNpgsql(connectionString,
+                    npgsqlOptions =>
+                        npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName,
+                            Schemas.Default))
+                .UseCamelCaseNamingConvention());
+
+        return services;
+    }
+}

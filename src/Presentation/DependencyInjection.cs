4e6ef54c01a2dd3109ce7b9a7a4b7772c7a8c69f
milewsk <milewski.konrad00@gmail.com>
2025-08-23 20:29:28 +0200

    Add localization to web api

--- src/Presentation/DependencyInjection.cs
+++ src/Presentation/DependencyInjection.cs
@@ -1,13 +1,22 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+
 using Presentation.Middlewares;
 
 namespace Presentation;
 
 public static class DependencyInjection
 {
+    private static readonly string[] ConfigureOptions = ["en-US", "pl-PL", "de-DE"];
+
     public static IServiceCollection AddPresentation(
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        services
+            .AddLocalization()
+            .AddControllers();
+
         return services;
     }
 
@@ -27,15 +36,50 @@ public static class DependencyInjection
         // Add request middlewares
         AddMiddlewares(app);
 
+        app.AddLocalization();
+
         app.MapControllers();
 
         return app;
     }
 
-
     private static WebApplication AddMiddlewares(this WebApplication app)
     {
         app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+        return app;
+    }
+
+
+    private static IServiceCollection AddLocalization(
+        this IServiceCollection services)
+    {
+        services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            var supportedCultures = ConfigureOptions;
+
+            options.SetDefaultCulture("en-US")
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            options.RequestCultureProviders.Insert(
+                0,
+                new AcceptLanguageHeaderRequestCultureProvider());
+        });
+
+        return services;
+    }
+
+
+    private static WebApplication AddLocalization(this WebApplication app)
+    {
+        var localizationOptions = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+        if (localizationOptions?.Value != null)
+        {
+            app.UseRequestLocalization(localizationOptions.Value);
+        }
 
         return app;
     }
